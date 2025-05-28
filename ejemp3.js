9851342d971e6a80b42545cc1110c9ba967f7d5f
Mon <chavezt.ms@gmail.com>
2025-05-28 12:16:52 -0700

    este ejercicio todavia no conluye

--- ejemp3.js
+++ ejemp3.js
@@ -8,8 +8,23 @@ resultado de diversas combinaciones lógicas.
 
 const prompt = require('prompt-sync')();
 
-let condicion1= prompt (' dame un valor verdadero o falso: ')
-let condicion2= prompt (' dame otro valor , verdaderi o falso: ')
+let condicion1=parseBoolean(prompt (' dame un valor verdadero o falso: '));
+let condicion2=parseBoolean(prompt (' dame otro valor , verdaderi o falso: '));
 
+if (condicion1===falso){
+    console.log(' falso, verdadero; verdadero,verdadero; falso, falso; verdadero falso')
 
+} else if ( condicion1 === verdadero ){
+    console.log (' verdadero, falso; verdadero,verdadero; falso, falso; falso,verdadedor')
 
+}else if ( condicion2===falso){
+    console.log (' falso, verdadero; verdadero,verdadero; falso, falso; verdadero falso' )
+
+}
+else if (condicion2===falso){
+    console.log (' verdadero, falso; verdadero,verdadero; falso, falso; falso,verdadedor')
+}
+else {
+console.log ('fin');
+}
+// monica esto esta todo mal, atte monica
